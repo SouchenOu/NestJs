4a1f94fdc65285529c6a9b0b512dc0f3f681c68e
Soukaina Ouchen <souchen@e3r9p1.1337.ma>
2023-08-25 15:45:05 +0100

    finish with validation

--- nest-project/src/users/controllers/users/users.controller.ts
+++ nest-project/src/users/controllers/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Req, Res , Body, Param, Query ,UsePipes, ValidationPipe} from '@nestjs/common';
+import { Controller, Get, Post, Req, Res , Body, Param, Query ,UsePipes, ValidationPipe, ParseIntPipe} from '@nestjs/common';
 import {Request,Response} from 'express'
 import { CreateUserDto } from 'src/users/dtos/createUserDto';
 
@@ -6,7 +6,7 @@ import { CreateUserDto } from 'src/users/dtos/createUserDto';
 // a controller is define a method 
 // this is the prefix for every route
 @Controller('users')
-/******************************************Get request**************************** */
+/******************************************Get request******************************************** */
 export class UsersController {
     // we want to use a decorator (the GET decorator)--> function decorator are basically just a function
     // we can pass an argument inside this Get() function to define the route for this endpoint
@@ -56,7 +56,9 @@ export class UsersController {
             },
         ];
     }
-    /***********************************Post request********************************* */
+    /******************************************Post request**************************************** */
+
+
     /*Post request are used for creating a resources on the backend server so for example, if you were to create a register form, 
     that register form where the user submits there  data it is supposed to make a post request to the server
     so to handle post request  that we would do the same thing with GET request we would define a method called createUser()*/
@@ -75,6 +77,8 @@ export class UsersController {
         response.send('created')
 
     }
+    /*********************************@Body() decorator*********************************** */
+
     /*******lets handle request bodies with nestjs way */
     /****we are gonna use a body decorator @body() that also imported from @nestjs/common  and then what we want to do is we want to name our parameter you can name it whatever you want.
      * okey now the next part we need to type-annotate our argument but what exactly do the type-annotate, so what you would  type-annotated with is what is called a data transfer object, so
@@ -92,15 +96,15 @@ export class UsersController {
 
     // }
 
-    /********route parameter*/
-    @Get(':id')
-    getUserById(@Param('id') id: string)
-    {
-        console.log(id);
-        return{id};
-    }
+    /*************************route parameter*******@Param()****************************/
+    // @Get(':id')
+    // getUserById(@Param('id') id: string)
+    // {
+    //     console.log(id);
+    //     return{id};
+    // }
 
-
+    /**************************************Query parameters************************************** */
 /*********Query parameters : with query parameters in express we will reference request dot query but in nestJs we have a decorated called query that allows you to extract the query parameters
      * the query parameters are best for filtering by the users (his email or username)
      * so query parameters are best used for doing actions such as feltering so for example you want to filter users based on what the first alphabets starts with, 
@@ -112,8 +116,8 @@ export class UsersController {
     {
         return [{username: 'fjk', email: "test1@gmail.com"},{username: "cde", email: "test2@gmail.com"}]
     }
-
-    /********a validation : a validation is something that is very very  important when you are building a web services, because if you send data to your server without validating it, you are going to run a lot of issues it could break your application.
+    /****************************************Validation************************************** */
+    /****************************************a validation : a validation is something that is very very  important when you are building a web services, because if you send data to your server without validating it, you are going to run a lot of issues it could break your application.
      * so it is so important to validate all of your request bodies 
         */
        /* so we will show how to validate the request body for our create user endpoint, and then we will show how we can validate other 
@@ -137,6 +141,15 @@ export class UsersController {
             console.log(userData)
             return {}
 
+        }
+
+        /****************validation ---> what if we want the user to ensure that they are providing numeric values for the id (validate that id should be a number nor string) */
+        //so we can actually get it to automaticly convert to the correct data type in our case we wanna convert it to a numeric value (by using ParceIntPipe())
+        @Get(':id')
+        getUserById(@Param('id', ParseIntPipe) id: number)
+        {
+            console.log(id);
+            return{id};
         }
 
 }
