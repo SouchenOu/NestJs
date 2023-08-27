ec9167de1dbe256ea03c053668569f50116cff53
Soukaina Ouchen <souchen@e3r9p1.1337.ma>
2023-08-27 12:48:10 +0100

    impliment all requests type and @params(), @Req

--- second-course-nest/src/user/user.controller.ts
+++ second-course-nest/src/user/user.controller.ts
@@ -1,10 +1,47 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Post, Patch, Delete , Req, Param} from "@nestjs/common";
 
 //each controller has more than one route, and different routes can perform different actions.
 @Controller('users')
 export class appController{
+
+    //The @Get() HTTP request method decorator before the findAll() method tells Nest to create a handler for a specific endpoint for HTTP requests. 
     @Get()
     findAllUsers() : string[] {
         return ['soukaina', 'ahmed', 'salma']
+    }
+
+    @Post()
+    Create(@Req() req: Request): string{
+        console.log(req.body);
+        return ("create new user");
+    }
+    @Post()
+    //We obviously must declare a method to bind the route to
+    createUser() : string
+    {
+        return "create user";
+    }
+    @Patch()
+    update() : string{
+        return "update user";
+    }
+
+    @Delete()
+    delete_user() : string{
+        return "delete user"
+    }
+
+
+    // @param
+    @Get(':id')
+    findOne(@Param("id") id: string) : string{
+        return id;
+
+    }
+
+    @Get(':username')
+    findTwo(@Param("username") username: number) : number{
+        return username;
+
     }
 }
